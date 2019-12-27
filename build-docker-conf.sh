095e3d4ffce935cf07013e998556621632d43fb0
Pontus <lmartins@pontusnetworks.com>
2019-12-27 12:44:12 +0000

    cleaning docker files

--- build-docker-conf.sh
+++ build-docker-conf.sh
@@ -3,7 +3,8 @@
 DIR="$( cd "$(dirname "$0")" ; pwd -P )"
 
 cd $DIR/docker-conf
-docker build --no-cache --rm . -t pontusvisiongdpr/pontus-graphdb-odb-conf
+#docker build --no-cache --rm . -t pontusvisiongdpr/pontus-graphdb-odb-conf
+docker build --no-cache    -f $DIR/docker-conf/Dockerfile -t pontusvisiongdpr/pontus-graphdb-odb-conf $DIR
 
 docker push pontusvisiongdpr/pontus-graphdb-odb-conf
 
