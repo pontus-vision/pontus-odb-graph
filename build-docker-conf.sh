88fce7c61613facdc56c491ca03e84ca4e02cd5e
Leo Martins <lmartins@pontusvision.com>
2020-07-23 12:55:08 +0100

    wip

--- build-docker-conf.sh
+++ build-docker-conf.sh
@@ -5,7 +5,7 @@ set -e
 
 cd $DIR/docker-conf
 #docker build --no-cache --rm . -t pontusvisiongdpr/pontus-graphdb-odb-conf
-docker build     -f $DIR/docker-conf/Dockerfile -t pontusvisiongdpr/pontus-graphdb-odb-conf $DIR
+docker build  --no-cache   -f $DIR/docker-conf/Dockerfile -t pontusvisiongdpr/pontus-graphdb-odb-conf $DIR
 
 docker push pontusvisiongdpr/pontus-graphdb-odb-conf
 
