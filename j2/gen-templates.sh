c59f30d188aa557bea39ab6a4eb13a88aefcb531
Leo Martins <lmartins@pontusvision.com>
2021-05-23 21:23:11 +0100

    wip

--- j2/gen-templates.sh
+++ j2/gen-templates.sh
@@ -9,11 +9,9 @@ yq . ../conf/lgpd-schema.yaml  >  ../conf/lgpd-schema.json
 
 cat vars/globals.yaml | jinja2 --strict -o ../conf/all-pole templates/all-pole.j2
 
-cat vars/*rules*.yaml | jinja2 --strict -o create-files.sh templates/create-files.sh.j2
-
-chmod 755 create-files.sh
-
-./create-files.sh
-
-rm ./create-files.sh
-
+for i in  vars/*rules*.yaml; do
+  jinja2 --strict -o create-files.sh templates/create-files.sh.j2 < "$i"
+  chmod 755 create-files.sh
+  ./create-files.sh "$i"
+  rm ./create-files.sh
+done
