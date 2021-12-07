855ad35172fe308d043afe9be1df7247e0d8332a
Omar Ghoche <oghoche@pontusvision.com>
2021-12-07 14:07:43 -0300

    * updated sharepoint_mapeamento_de_processo with new prop Object.Data_Procedures.Interested_Parties_Consulted  * added new prop Interested_Parties_Consulted @ globals.yaml  * updated the RIPD html template section 4 to acomodate the new Partes Interessadas Consultadas  * Added new test for mapeamento de processo test00003SharepointMapeamentoDeProcesso @ PVTemplateTests.java

--- src/test/java/com/pontusvision/gdpr/PVTemplateTests.java
+++ src/test/java/com/pontusvision/gdpr/PVTemplateTests.java
@@ -15,6 +15,7 @@ import java.time.format.DateTimeFormatter;
 import java.util.Base64;
 import java.util.Date;
 import java.util.Locale;
+import java.util.concurrent.ExecutionException;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNull;
@@ -189,4 +190,36 @@ public class PVTemplateTests extends AppTest {
 
   }
 
+  @Test
+  public void test00003SharepointMapeamentoDeProcesso() throws InterruptedException {
+    try {
+      jsonTestUtil("pv-extract-sharepoint-mapeamento-de-processo.json",
+              "$.queryResp[*].fields","sharepoint_mapeamentos");
+
+      String EPGInterest =
+              App.executor.eval("App.g.V().has('Object.Data_Policy.Type', eq('6')).in('Has_Policy')" +
+                      ".properties('Object.Data_Procedures.Interested_Parties_Consulted').value().next().toString()").get().toString();
+      assertEquals("EPG Advogados",EPGInterest, "EPG Advogados is a Party Interested in these Data Procedures");
+
+      String numEPGParties =
+              App.executor.eval("App.g.V().has('Object.Data_Procedures.Interested_Parties_Consulted', " +
+                      "eq('EPG Advogados')).count().next().toString()").get().toString();
+      assertEquals("2",numEPGParties, "2 registries have EPG Advogados as an Interested Party");
+
+      String juridicoInterest =
+              App.executor.eval("App.g.V().has('Object.Lawful_Basis.Description', eq('LEGÍTIMO INTERESSE DO CONTROLADOR'))" +
+                      ".in('Has_Lawful_Basis_On').properties('Object.Data_Procedures.Interested_Parties_Consulted').value().next().toString()").get().toString();
+      assertEquals("Jurídico Sunnyvale",juridicoInterest, "Jurídico Sunnyvale is a Party Interested in these Data Procedures");
+
+      String numJuridicoParties =
+              App.executor.eval("App.g.V().has('Object.Data_Procedures.Interested_Parties_Consulted', " +
+                      "eq('Jurídico Sunnyvale')).count().next().toString()").get().toString();
+      assertEquals("4",numJuridicoParties, "4 registries have Jurídico Sunnyvale as an Interested Party");
+
+    } catch (ExecutionException e) {
+      e.printStackTrace();
+      assertNull(e);
+
+    }
+  }
 }
