d518e405c2283f9abe649f153ab5fa749f654e16
Omar Mohamad Abou Ghoche <omarghoucher@gmail.com>
2022-01-11 09:30:54 -0300

    added build-docker-lib.sh

---
+++ .github/build-docker-lib.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+export TAG=${TAG:-1.13.2}
+export DOLLAR='$'
+DIR="$( cd "$(dirname "$0")" ; pwd -P )"
+set -e
+#cd $DIR/docker-lib
+#docker build --no-cache  --rm . -t pontusvisiongdpr/pontus-graphdb-odb-lib
+export CURR_TAG=${CURR_TAG:-$TAG}
+cat $DIR/docker-lib/Dockerfile.template | envsubst > $DIR/docker-lib/Dockerfile
+docker build --progress=plain --rm  -f $DIR/docker-lib/Dockerfile -t pontusvisiongdpr/pontus-graphdb-odb-lib:${CURR_TAG} $DIR
+docker push pontusvisiongdpr/pontus-graphdb-odb-lib:${CURR_TAG}
