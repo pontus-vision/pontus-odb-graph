40d73a39d5369cfb5ed233e75a1b751a34853e5e
Omar Ghoche <oghoche@pontusvision.com>
2022-01-18 18:56:56 -0300

    Created POLE sharepoint_consents && updates sharepoint_privacy_notice

--- src/test/java/com/pontusvision/gdpr/PVSharepointRoPaTests.java
+++ src/test/java/com/pontusvision/gdpr/PVSharepointRoPaTests.java
@@ -10,6 +10,7 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestClassOrder;
 import org.junit.jupiter.api.TestMethodOrder;
 
+import java.text.ParseException;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.util.Base64;
@@ -17,8 +18,7 @@ import java.util.Date;
 import java.util.Locale;
 import java.util.concurrent.ExecutionException;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.*;
 
 //import static org.junit.Assert.assertEquals;
 //import static org.junit.Assert.assertNull;
@@ -101,40 +101,216 @@ public class PVSharepointRoPaTests extends AppTest {
               "$.queryResp[*].fields", "sharepoint_privacy_notice");
 
       String getPrivacyNoticeDescription =
-              App.executor.eval("App.g.V().has('Object.Privacy_Notice.Form_Id', eq('abc'))" +
+              App.executor.eval("App.g.V().has('Object.Privacy_Notice.Form_Id', eq('1'))" +
                       ".properties('Object.Privacy_Notice.Description').value()" +
                       ".next().toString()").get().toString();
       assertEquals("O Titular foi avisado e concordou com os termos", getPrivacyNoticeDescription,
-              "Privacy Notice's Description for Title 'abc'");
+              "Privacy Notice's Description for Title 'abc'.");
 
       String getPrivacyNoticeCreateDate =
-              App.executor.eval("App.g.V().has('Object.Privacy_Notice.Form_Id', eq('mno'))" +
+              App.executor.eval("App.g.V().has('Object.Privacy_Notice.Form_Id', eq('5'))" +
                       ".properties('Object.Privacy_Notice.Metadata.Create_Date').value()" +
                       ".next().toString()").get().toString();
-      getPrivacyNoticeCreateDate = getPrivacyNoticeCreateDate.replaceAll("... 2022", "GMT 2022");
-      assertEquals("Wed Jan 05 16:18:05 GMT 2022", getPrivacyNoticeCreateDate,
-              "Privacy Notice's creation date");
+//      getPrivacyNoticeCreateDate = getPrivacyNoticeCreateDate.replaceAll("... 2022", "GMT 2022");
+      assertEquals(dtfmt.parse("Wed Jan 05 16:18:05 BRT 2022"), dtfmt.parse(getPrivacyNoticeCreateDate),
+              "Privacy Notice's creation date.");
 
       String getPrivacyNoticeCountryWhereStored =
-              App.executor.eval("App.g.V().has('Object.Privacy_Notice.Form_Id', eq('def'))" +
+              App.executor.eval("App.g.V().has('Object.Privacy_Notice.Form_Id', eq('2'))" +
                       ".properties('Object.Privacy_Notice.Country_Where_Stored').value()" +
                       ".next().toString()").get().toString();
       assertEquals("Brazil", getPrivacyNoticeCountryWhereStored,
-              "Privacy Notice's Data's Country Storage");
+              "Privacy Notice's Data's Country Storage.");
 
       String getPrivacyNoticeWhoIsCollecting =
-              App.executor.eval("App.g.V().has('Object.Privacy_Notice.Form_Id', eq('jkl'))" +
+              App.executor.eval("App.g.V().has('Object.Privacy_Notice.Form_Id', eq('4'))" +
                       ".properties('Object.Privacy_Notice.Who_Is_Collecting').value()" +
                       ".next().toString()").get().toString();
       assertEquals("Sxxxxxxxe", getPrivacyNoticeWhoIsCollecting,
-              "Privacy Notice's Data's Company Collector");
+              "Privacy Notice's Data's Company Collector.");
 
       String getObjectDataSourceName =
-              App.executor.eval("App.g.V().has('Object.Privacy_Notice.Form_Id', eq('jkl')).out('Has_Ingestion_Event')" +
+              App.executor.eval("App.g.V().has('Object.Privacy_Notice.Form_Id', eq('4')).out('Has_Ingestion_Event')" +
                       ".in('Has_Ingestion_Event').in('Has_Ingestion_Event').properties('Object.Data_Source.Name').value()" +
                       ".next().toString()").get().toString();
-      assertEquals("SHAREPOINT/PRIVACY-NOTICE", getObjectDataSourceName,
-              "Privacy Notice's Data's Company Collector");
+      assertEquals("SHAREPOINT/PRIVACY-NOTICE", getObjectDataSourceName, "Data Source Name.");
+
+      String getPrivacyNoticeDeliveryDate =
+              App.executor.eval("App.g.V().has('Object.Privacy_Notice.Form_Id', eq('7'))" +
+                      ".properties('Object.Privacy_Notice.Delivery_Date').value()" +
+                      ".next().toString()").get().toString();
+      assertEquals(dtfmt.parse("Fri Dec 10 22:01:01 BRT 2021"), dtfmt.parse(getPrivacyNoticeDeliveryDate), "Privacy Notice's delivery date for id 'stuvw'.");
+
+    } catch (ExecutionException | ParseException e) {
+      e.printStackTrace();
+      assertNull(e);
+    }
+
+  }
+
+  // Testing for upsert of two similar json data using sharepoint_privacy_notice POLE
+  @Test
+  public void test00003UpsertSharepointPrivacyNotice() throws InterruptedException {
+    try {
+      jsonTestUtil("non-official-pv-extract-sharepoint-aviso-de-privacidade.json",
+              "$.queryResp[*].fields", "sharepoint_privacy_notice");
+
+      String countEventIngestions =
+              App.executor.eval("App.g.V().has('Event.Ingestion.Type', eq('Privacy Notice'))" +
+                      ".count().next().toString()").get().toString();
+
+      String countPrivacyNotices =
+              App.executor.eval("App.g.V().has('Event.Ingestion.Type', eq('Privacy Notice'))" +
+                      ".in('Has_Ingestion_Event').has('Metadata.Type.Object.Privacy_Notice', eq('Object.Privacy_Notice'))" +
+                      ".dedup().count().next().toString()").get().toString();
+
+      jsonTestUtil("non-official-pv-extract-sharepoint-aviso-de-privacidade2.json",
+              "$.queryResp[*].fields", "sharepoint_privacy_notice");
+
+      String countEventIngestionsAgain =
+              App.executor.eval("App.g.V().has('Event.Ingestion.Type', eq('Privacy Notice'))" +
+                      ".count().next().toString()").get().toString();
+
+//      Test for duplicate data
+
+      String countPrivacyNoticesAgain =
+              App.executor.eval("App.g.V().has('Event.Ingestion.Type', eq('Privacy Notice'))" +
+                      ".in('Has_Ingestion_Event').has('Metadata.Type.Object.Privacy_Notice', eq('Object.Privacy_Notice'))" +
+                      ".dedup().count().next().toString()").get().toString();
+
+//    This proves that new insertions were made the Privacy Notice Graph part
+      assertTrue(Integer.parseInt(countEventIngestionsAgain) > Integer.parseInt(countEventIngestions));
+
+//    This proves that data is still the same
+      assertTrue(Integer.parseInt(countPrivacyNotices) == Integer.parseInt(countPrivacyNoticesAgain));
+
+    } catch (ExecutionException e) {
+      e.printStackTrace();
+      assertNull(e);
+    }
+
+  }
+
+  @Test
+  public void test00004SharepointConsents() throws InterruptedException {
+    try {
+      jsonTestUtil("non-official-pv-extract-sharepoint-consentimentos.json",
+              "$.queryResp[*].fields", "sharepoint_consents");
+
+      String lakshmiPrivacyNotices =
+              App.executor.eval("App.g.V().has('Person.Natural.Full_Name', eq('LAKSHMI')).out('Consent')" +
+                      ".out('Has_Privacy_Notice').count().next().toString()").get().toString();
+      assertEquals("0", lakshmiPrivacyNotices, "Lakshmi has NO Privacy notices.");
+
+      String janosPrivacyNotices =
+              App.executor.eval("App.g.V().has('Person.Natural.Full_Name', eq('JANOS GÁBOR')).out('Consent')" +
+                      ".out('Has_Privacy_Notice').count().next().toString()").get().toString();
+      assertEquals("2", janosPrivacyNotices, "Janos has 2 Privacy notices.");
+
+      String laraPrivacyNotices =
+              App.executor.eval("App.g.V().has('Person.Natural.Full_Name', eq('LARA CROFT')).out('Consent')" +
+                      ".out('Has_Privacy_Notice').count().next().toString()").get().toString();
+      assertEquals("1", laraPrivacyNotices, "Lara has one Privacy notices.");
+
+      String getObjectDataSourceName =
+              App.executor.eval("App.g.V().has('Object.Privacy_Notice.Form_Id', eq('4')).in('Has_Privacy_Notice')" +
+                      ".in('Consent').out('Has_Ingestion_Event').in('Has_Ingestion_Event').in('Has_Ingestion_Event')" +
+                      ".properties('Object.Data_Source.Name').value().next().toString()").get().toString();
+      assertEquals("SHAREPOINT/CONSENT", getObjectDataSourceName, "Data Source Name.");
+
+      String getPersonNaturalCustomerID =
+              App.executor.eval("App.g.V().has('Event.Consent.Form_Id', eq('1')).in('Consent')" +
+                      ".properties('Person.Natural.Customer_ID').value().next().toString()").get().toString();
+      assertEquals("123", getPersonNaturalCustomerID, "Karla's Customer Id Number");
+
+    } catch (ExecutionException e) {
+      e.printStackTrace();
+      assertNull(e);
+    }
+
+  }
+
+  // Testing for upsert of two similar json data using sharepoint_consents POLE
+  @Test
+  public void test00005UpsertSharepointConsents() throws InterruptedException {
+    try {
+      jsonTestUtil("non-official-pv-extract-sharepoint-consentimentos.json",
+              "$.queryResp[*].fields", "sharepoint_consents");
+
+      String countEventIngestions =
+              App.executor.eval("App.g.V().has('Event.Ingestion.Type', eq('Consent'))" +
+                      ".count().next().toString()").get().toString();
+
+      String countPersonNatural =
+              App.executor.eval("App.g.V().has('Event.Ingestion.Type', eq('Consent'))" +
+                      ".in('Has_Ingestion_Event').has('Metadata.Type.Person.Natural', eq('Person.Natural'))" +
+                      ".dedup().count().next().toString()").get().toString();
+
+      String countConsentEvents =
+              App.executor.eval("App.g.V().has('Event.Ingestion.Type', eq('Consent'))" +
+                      ".in('Has_Ingestion_Event').has('Metadata.Type.Person.Natural', eq('Person.Natural'))" +
+                      ".out('Consent').dedup().count().next().toString()").get().toString();
+
+//    Test for duplicate data --------------------------------------------------------------------------------------
+
+      jsonTestUtil("non-official-pv-extract-sharepoint-consentimentos2.json",
+              "$.queryResp[*].fields", "sharepoint_consents");
+
+      String countEventIngestionsAgain =
+              App.executor.eval("App.g.V().has('Event.Ingestion.Type', eq('Consent'))" +
+                      ".count().next().toString()").get().toString();
+
+      String countPersonNaturalAgain =
+              App.executor.eval("App.g.V().has('Event.Ingestion.Type', eq('Consent'))" +
+                      ".in('Has_Ingestion_Event').has('Metadata.Type.Person.Natural', eq('Person.Natural'))" +
+                      ".dedup().count().next().toString()").get().toString();
+
+      String countConsentEventsAgain =
+              App.executor.eval("App.g.V().has('Event.Ingestion.Type', eq('Consent'))" +
+                      ".in('Has_Ingestion_Event').has('Metadata.Type.Person.Natural', eq('Person.Natural'))" +
+                      ".out('Consent').dedup().count().next().toString()").get().toString();
+
+//    This proves that new insertions were made the Privacy Notice Graph part
+      assertTrue(Integer.parseInt(countEventIngestionsAgain) > Integer.parseInt(countEventIngestions));
+
+//    This proves that data is still the same
+      assertTrue(Integer.parseInt(countPersonNatural) == Integer.parseInt(countPersonNaturalAgain));
+      assertTrue(Integer.parseInt(countConsentEvents) == Integer.parseInt(countConsentEventsAgain));
+
+    } catch (ExecutionException e) {
+      e.printStackTrace();
+      assertNull(e);
+    }
+
+  }
+
+  @Test
+  public void test00006SharepointConsentPlusPrivacyNotice() throws InterruptedException {
+    try {
+      jsonTestUtil("non-official-pv-extract-sharepoint-aviso-de-privacidade.json",
+              "$.queryResp[*].fields", "sharepoint_privacy_notice");
+
+      jsonTestUtil("non-official-pv-extract-sharepoint-consentimentos.json",
+              "$.queryResp[*].fields", "sharepoint_consents");
+
+      String consentDataSourceName = "SHAREPOINT/CONSENT";
+
+      String privacyNoticeDataSourceName =
+              App.executor.eval("App.g.V().has('Object.Data_Source.Name', eq(\"" + consentDataSourceName + "\"))" +
+                      ".out('Has_Ingestion_Event').out('Has_Ingestion_Event').in('Has_Ingestion_Event').out('Consent')" +
+                      ".out('Has_Privacy_Notice').out('Has_Ingestion_Event').in('Has_Ingestion_Event')" +
+                      ".in('Has_Ingestion_Event').values('Object.Data_Source.Name').next().toString()").get().toString();
+      assertEquals("SHAREPOINT/PRIVACY-NOTICE", privacyNoticeDataSourceName, "Data Source Name for Privacy Notice POLE.");
+
+      String getPersonNaturalByPrivacyNotice =
+              App.executor.eval("App.g.V().has('Object.Data_Source.Name', eq(\"" + privacyNoticeDataSourceName + "\"))" +
+                      ".out('Has_Ingestion_Event').out('Has_Ingestion_Event').in('Has_Ingestion_Event')" +
+                      ".has('Object.Privacy_Notice.Form_Id', eq('4')).in('Has_Privacy_Notice')" +
+                      ".has('Event.Consent.Customer_ID', eq('456'))" +
+                      ".in('Consent').has('Person.Natural.Customer_ID', eq('456'))" +
+                      ".values('Person.Natural.Full_Name').next().toString()").get().toString();
+      assertEquals("BOB NAKAMURA", getPersonNaturalByPrivacyNotice,
+              "This Privacy Notice POLE Graph path leads to Bob Nakamura");
 
     } catch (ExecutionException e) {
       e.printStackTrace();
