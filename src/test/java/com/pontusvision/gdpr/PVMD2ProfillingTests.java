4998fb4f0859cca21544605ee6c10909ef766d44
Omar Ghoche <oghoche@pontusvision.com>
2022-01-27 19:51:03 -0300

    MD2 ingestion "profilling"

--- src/test/java/com/pontusvision/gdpr/PVMD2ProfillingTests.java
+++ src/test/java/com/pontusvision/gdpr/PVMD2ProfillingTests.java
@@ -1,15 +1,12 @@
 package com.pontusvision.gdpr;
 
-import com.pontusvision.graphutils.VisJSGraph;
-import org.json.JSONArray;
-import org.json.JSONObject;
+import com.google.common.base.Stopwatch;
 import org.junit.jupiter.api.MethodOrderer;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestClassOrder;
 import org.junit.jupiter.api.TestMethodOrder;
 
-import java.util.*;
-import java.util.stream.Collectors;
+import java.util.concurrent.TimeUnit;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNull;
@@ -33,16 +30,67 @@ public class PVMD2ProfillingTests extends AppTest {
    * @return the suite of test0000s being test0000ed
    */
 
+//  Ingestion of 500 records in CSV format in constant batches of 100
   @Test
-  public void test00001MD2Profilling() throws InterruptedException {
+  public void test00001MD2Profiling() throws InterruptedException {
     try {
 
-      csvTestUtil("md2-profilling.csv", "pv_md2");
+      String[] csvFiles =
+              {"md2/md2-profiling0.csv", "md2/md2-profiling1.csv", "md2/md2-profiling2.csv", "md2/md2-profiling3.csv", "md2/md2-profiling4.csv"};
 
-      String adaAlexander =
-              App.executor.eval("App.g.V().has('Object.Identity_Card.Id_Value',eq('63152378866')).in('Has_Id_Card')" +
+      for (String csvFile : csvFiles) {
+
+        Stopwatch stopwatch = Stopwatch.createStarted();
+
+        System.out.println("-----------------------------------------------------------------------------------------");
+
+        csvTestUtil(csvFile, "pv_md2");
+
+        stopwatch.stop(); // optional
+        System.out.println("Time elapsed for file " + csvFile + " = " + stopwatch.elapsed(TimeUnit.SECONDS) + " seconds");
+      }
+
+      System.out.println("-----------------------------------------------------------------------------------------");
+
+      String aquinoRodrigues =
+              App.executor.eval("App.g.V().has('Object.Identity_Card.Id_Value',eq('9468285804')).in('Has_Id_Card')" +
                       ".values('Person.Natural.Full_Name').next().toString()").get().toString();
-      assertEquals("ADA ALEXANDER", adaAlexander, "This CPF belongs to Ada Alexander");
+      assertEquals("AQUINO RODRIGUES", aquinoRodrigues, "This CPF belongs to Aquino Rodrigues");
+
+      String alfordHiggins =
+              App.executor.eval("App.g.V().has('Object.Identity_Card.Id_Value',eq('96908039994')).in('Has_Id_Card')" +
+                      ".values('Person.Natural.Full_Name').next().toString()").get().toString();
+      assertEquals("ALFORD HIGGINS", alfordHiggins, "This CPF belongs to Alford Higgins");
+
+      String lennyCooper =
+              App.executor.eval("App.g.V().has('Object.Identity_Card.Id_Value',eq('33666669352')).in('Has_Id_Card')" +
+                      ".values('Person.Natural.Full_Name').next().toString()").get().toString();
+      assertEquals("LENNY COOPER", lennyCooper, "This CPF belongs to Lenny Cooper");
+
+      String deliaMonteiro =
+              App.executor.eval("App.g.V().has('Object.Email_Address.Email',eq('delia.monteiro@example.com')).in('Uses_Email')" +
+                      ".values('Person.Natural.Full_Name').next().toString()").get().toString();
+      assertEquals("DÃ©LIA MONTEIRO", deliaMonteiro, "This CPF belongs to DÃ©lia Monteiro");
+
+      String miriamDaLuz =
+              App.executor.eval("App.g.V().has('Object.Email_Address.Email',eq('miriam.daluz@example.com')).in('Uses_Email')" +
+                      ".values('Person.Natural.Full_Name').next().toString()").get().toString();
+      assertEquals("MIRIAM DA LUZ", miriamDaLuz, "This CPF belongs to Miriam da Luz");
+
+//      { "status": "success", "successCount": 100 }
+//      Time elapsed for file md2/md2-profiling0.csv = 12 seconds
+//      -----------------------------------------------------------------------------------------
+//      { "status": "success", "successCount": 100 }
+//      Time elapsed for file md2/md2-profiling1.csv = 6 seconds
+//      -----------------------------------------------------------------------------------------
+//      { "status": "success", "successCount": 100 }
+//      Time elapsed for file md2/md2-profiling2.csv = 7 seconds
+//      -----------------------------------------------------------------------------------------
+//      { "status": "success", "successCount": 100 }
+//      Time elapsed for file md2/md2-profiling3.csv = 8 seconds
+//      -----------------------------------------------------------------------------------------
+//      { "status": "success", "successCount": 100 }
+//      Time elapsed for file md2/md2-profiling4.csv = 9 seconds
 
     } catch (Exception e) {
       e.printStackTrace();
@@ -52,5 +100,236 @@ public class PVMD2ProfillingTests extends AppTest {
 
 
   }
+
+  //  Ingestion of 1000 records in CSV format in constant batches of 100
+  @Test
+  public void test00002MD2Profiling() throws InterruptedException {
+    try {
+
+      String[] csvFiles =
+              {"md2/md2-profiling0.csv", "md2/md2-profiling1.csv", "md2/md2-profiling2.csv", "md2/md2-profiling3.csv", "md2/md2-profiling4.csv",
+                      "md2/md2-profiling5.csv", "md2/md2-profiling6.csv", "md2/md2-profiling7.csv", "md2/md2-profiling8.csv", "md2/md2-profiling9.csv"};
+
+      for (String csvFile : csvFiles) {
+
+        Stopwatch stopwatch = Stopwatch.createStarted();
+
+        System.out.println("-----------------------------------------------------------------------------------------");
+
+        csvTestUtil(csvFile, "pv_md2");
+
+        stopwatch.stop(); // optional
+        System.out.println("Time elapsed for file " + csvFile + " = " + stopwatch.elapsed(TimeUnit.SECONDS) + " seconds");
+      }
+
+      System.out.println("-----------------------------------------------------------------------------------------");
+
+      String aquinoRodrigues =
+              App.executor.eval("App.g.V().has('Object.Identity_Card.Id_Value',eq('9468285804')).in('Has_Id_Card')" +
+                      ".values('Person.Natural.Full_Name').next().toString()").get().toString();
+      assertEquals("AQUINO RODRIGUES", aquinoRodrigues, "This CPF belongs to Aquino Rodrigues");
+
+      String alfordHiggins =
+              App.executor.eval("App.g.V().has('Object.Identity_Card.Id_Value',eq('96908039994')).in('Has_Id_Card')" +
+                      ".values('Person.Natural.Full_Name').next().toString()").get().toString();
+      assertEquals("ALFORD HIGGINS", alfordHiggins, "This CPF belongs to Alford Higgins");
+
+      String lennyCooper =
+              App.executor.eval("App.g.V().has('Object.Identity_Card.Id_Value',eq('33666669352')).in('Has_Id_Card')" +
+                      ".values('Person.Natural.Full_Name').next().toString()").get().toString();
+      assertEquals("LENNY COOPER", lennyCooper, "This CPF belongs to Lenny Cooper");
+
+      String deliaMonteiro =
+              App.executor.eval("App.g.V().has('Object.Email_Address.Email',eq('delia.monteiro@example.com')).in('Uses_Email')" +
+                      ".values('Person.Natural.Full_Name').next().toString()").get().toString();
+      assertEquals("DÃ©LIA MONTEIRO", deliaMonteiro, "This CPF belongs to DÃ©lia Monteiro");
+
+      String miriamDaLuz =
+              App.executor.eval("App.g.V().has('Object.Email_Address.Email',eq('miriam.daluz@example.com')).in('Uses_Email')" +
+                      ".values('Person.Natural.Full_Name').next().toString()").get().toString();
+      assertEquals("MIRIAM DA LUZ", miriamDaLuz, "This CPF belongs to Miriam da Luz");
+
+      String jordinaAraujo =
+              App.executor.eval("App.g.V().has('Object.Identity_Card.Id_Value',eq('72714206140')).in('Has_Id_Card')" +
+                      ".values('Person.Natural.Full_Name').next().toString()").get().toString();
+      assertEquals("JORDINA ARAÃºJO", jordinaAraujo, "This CPF belongs to Jordina AraÃºjo");
+
+      String janeTeixeira =
+              App.executor.eval("App.g.V().has('Object.Identity_Card.Id_Value',eq('84478523770')).in('Has_Id_Card')" +
+                      ".values('Person.Natural.Full_Name').next().toString()").get().toString();
+      assertEquals("JÃ³NI TEIXEIRA", janeTeixeira, "This CPF belongs to JÃ³ni Teixeira");
+
+      String guiliaRamones =
+              App.executor.eval("App.g.V().has('Object.Identity_Card.Id_Value',eq('01411383605')).in('Has_Id_Card')" +
+                      ".values('Person.Natural.Full_Name').next().toString()").get().toString();
+      assertEquals("GUILIA RAMONES", guiliaRamones, "This CPF belongs to Guilia Ramones");
+
+      String safiraCosta =
+              App.executor.eval("App.g.V().has('Object.Identity_Card.Id_Value',eq('26491059847')).in('Has_Id_Card')" +
+                      ".values('Person.Natural.Full_Name').next().toString()").get().toString();
+      assertEquals("SAFIRA DA COSTA", safiraCosta, "This CPF belongs to Safira da Costa");
+
+      String rodrigoRodrigues =
+              App.executor.eval("App.g.V().has('Object.Identity_Card.Id_Value',eq('90052103625')).in('Has_Id_Card')" +
+                      ".values('Person.Natural.Full_Name').next().toString()").get().toString();
+      assertEquals("RODRIGO RODRIGUES", rodrigoRodrigues, "This CPF belongs to Rodrigo Rodrigues");
+
+//      { "status": "success", "successCount": 100 }
+//      Time elapsed for file md2/md2-profiling0.csv = 11 seconds
+//      -----------------------------------------------------------------------------------------
+//      { "status": "success", "successCount": 100 }
+//      Time elapsed for file md2/md2-profiling1.csv = 5 seconds
+//      -----------------------------------------------------------------------------------------
+//      { "status": "success", "successCount": 100 }
+//      Time elapsed for file md2/md2-profiling2.csv = 6 seconds
+//      -----------------------------------------------------------------------------------------
+//      { "status": "success", "successCount": 100 }
+//      Time elapsed for file md2/md2-profiling3.csv = 7 seconds
+//      -----------------------------------------------------------------------------------------
+//      { "status": "success", "successCount": 100 }
+//      Time elapsed for file md2/md2-profiling4.csv = 7 seconds
+//      -----------------------------------------------------------------------------------------
+//      { "status": "success", "successCount": 100 }
+//      Time elapsed for file md2/md2-profiling5.csv = 9 seconds
+//      -----------------------------------------------------------------------------------------
+//      { "status": "success", "successCount": 100 }
+//      Time elapsed for file md2/md2-profiling6.csv = 8 seconds
+//      -----------------------------------------------------------------------------------------
+//      { "status": "success", "successCount": 100 }
+//      Time elapsed for file md2/md2-profiling7.csv = 9 seconds
+//      -----------------------------------------------------------------------------------------
+//      { "status": "success", "successCount": 101 }
+//      Time elapsed for file md2/md2-profiling8.csv = 11 seconds
+//      -----------------------------------------------------------------------------------------
+//      { "status": "success", "successCount": 100 }
+//      Time elapsed for file md2/md2-profiling9.csv = 11 seconds
+
+    } catch (Exception e) {
+      e.printStackTrace();
+      assertNull(e);
+
+    }
+
+
+  }
+
+//  Ingestion of 500 records in CSV format in variable batches
+@Test
+public void test00003MD2Profiling() throws InterruptedException {
+  try {
+
+    String[] csvFiles =
+            {"md2/md2-profiling0.csv", "md2/md2-profiling1.csv", "md2/md2-profiling2.csv", "md2/md2-profiling3.csv", "md2/md2-profiling4.csv"};
+
+    for (String csvFile : csvFiles) {
+
+      Stopwatch stopwatch = Stopwatch.createStarted();
+
+      System.out.println("-----------------------------------------------------------------------------------------");
+
+      csvTestUtil(csvFile, "pv_md2");
+
+      stopwatch.stop(); // optional
+      System.out.println("Time elapsed for file " + csvFile + " = " + stopwatch.elapsed(TimeUnit.SECONDS) + " seconds");
+    }
+
+    System.out.println("-----------------------------------------------------------------------------------------");
+
+    String aquinoRodrigues =
+            App.executor.eval("App.g.V().has('Object.Identity_Card.Id_Value',eq('9468285804')).in('Has_Id_Card')" +
+                    ".values('Person.Natural.Full_Name').next().toString()").get().toString();
+    assertEquals("AQUINO RODRIGUES", aquinoRodrigues, "This CPF belongs to Aquino Rodrigues");
+
+    String alfordHiggins =
+            App.executor.eval("App.g.V().has('Object.Identity_Card.Id_Value',eq('96908039994')).in('Has_Id_Card')" +
+                    ".values('Person.Natural.Full_Name').next().toString()").get().toString();
+    assertEquals("ALFORD HIGGINS", alfordHiggins, "This CPF belongs to Alford Higgins");
+
+    String lennyCooper =
+            App.executor.eval("App.g.V().has('Object.Identity_Card.Id_Value',eq('33666669352')).in('Has_Id_Card')" +
+                    ".values('Person.Natural.Full_Name').next().toString()").get().toString();
+    assertEquals("LENNY COOPER", lennyCooper, "This CPF belongs to Lenny Cooper");
+
+    String deliaMonteiro =
+            App.executor.eval("App.g.V().has('Object.Email_Address.Email',eq('delia.monteiro@example.com')).in('Uses_Email')" +
+                    ".values('Person.Natural.Full_Name').next().toString()").get().toString();
+    assertEquals("DÃ©LIA MONTEIRO", deliaMonteiro, "This CPF belongs to DÃ©lia Monteiro");
+
+    String miriamDaLuz =
+            App.executor.eval("App.g.V().has('Object.Email_Address.Email',eq('miriam.daluz@example.com')).in('Uses_Email')" +
+                    ".values('Person.Natural.Full_Name').next().toString()").get().toString();
+    assertEquals("MIRIAM DA LUZ", miriamDaLuz, "This CPF belongs to Miriam da Luz");
+
+    String jordinaAraujo =
+            App.executor.eval("App.g.V().has('Object.Identity_Card.Id_Value',eq('72714206140')).in('Has_Id_Card')" +
+                    ".values('Person.Natural.Full_Name').next().toString()").get().toString();
+    assertEquals("JORDINA ARAÃºJO", jordinaAraujo, "This CPF belongs to Jordina AraÃºjo");
+
+    String janeTeixeira =
+            App.executor.eval("App.g.V().has('Object.Identity_Card.Id_Value',eq('84478523770')).in('Has_Id_Card')" +
+                    ".values('Person.Natural.Full_Name').next().toString()").get().toString();
+    assertEquals("JÃ³NI TEIXEIRA", janeTeixeira, "This CPF belongs to JÃ³ni Teixeira");
+
+    String guiliaRamones =
+            App.executor.eval("App.g.V().has('Object.Identity_Card.Id_Value',eq('01411383605')).in('Has_Id_Card')" +
+                    ".values('Person.Natural.Full_Name').next().toString()").get().toString();
+    assertEquals("GUILIA RAMONES", guiliaRamones, "This CPF belongs to Guilia Ramones");
+
+    String safiraCosta =
+            App.executor.eval("App.g.V().has('Object.Identity_Card.Id_Value',eq('26491059847')).in('Has_Id_Card')" +
+                    ".values('Person.Natural.Full_Name').next().toString()").get().toString();
+    assertEquals("SAFIRA DA COSTA", safiraCosta, "This CPF belongs to Safira da Costa");
+
+    String rodrigoRodrigues =
+            App.executor.eval("App.g.V().has('Object.Identity_Card.Id_Value',eq('90052103625')).in('Has_Id_Card')" +
+                    ".values('Person.Natural.Full_Name').next().toString()").get().toString();
+    assertEquals("RODRIGO RODRIGUES", rodrigoRodrigues, "This CPF belongs to Rodrigo Rodrigues");
+
+//      { "status": "success", "successCount": 100 }
+//      Time elapsed for file md2/md2-profiling0.csv = 11 seconds
+//      -----------------------------------------------------------------------------------------
+//      { "status": "success", "successCount": 100 }
+//      Time elapsed for file md2/md2-profiling1.csv = 5 seconds
+//      -----------------------------------------------------------------------------------------
+//      { "status": "success", "successCount": 100 }
+//      Time elapsed for file md2/md2-profiling2.csv = 6 seconds
+//      -----------------------------------------------------------------------------------------
+//      { "status": "success", "successCount": 100 }
+//      Time elapsed for file md2/md2-profiling3.csv = 7 seconds
+//      -----------------------------------------------------------------------------------------
+//      { "status": "success", "successCount": 100 }
+//      Time elapsed for file md2/md2-profiling4.csv = 7 seconds
+//      -----------------------------------------------------------------------------------------
+//      { "status": "success", "successCount": 100 }
+//      Time elapsed for file md2/md2-profiling5.csv = 9 seconds
+//      -----------------------------------------------------------------------------------------
+//      { "status": "success", "successCount": 100 }
+//      Time elapsed for file md2/md2-profiling6.csv = 8 seconds
+//      -----------------------------------------------------------------------------------------
+//      { "status": "success", "successCount": 100 }
+//      Time elapsed for file md2/md2-profiling7.csv = 9 seconds
+//      -----------------------------------------------------------------------------------------
+//      { "status": "success", "successCount": 101 }
+//      Time elapsed for file md2/md2-profiling8.csv = 11 seconds
+//      -----------------------------------------------------------------------------------------
+//      { "status": "success", "successCount": 100 }
+//      Time elapsed for file md2/md2-profiling9.csv = 11 seconds
+
+  } catch (Exception e) {
+    e.printStackTrace();
+    assertNull(e);
+
+  }
+
+
+}
+
+//  TODO:
+
+//  Ingestion of 500 records in JSON format in constant batches of 100
+
+//  Ingestion of 1000 records in JSON format in constant batches of 100
+
+//  Ingestion of 500 records in JSON format in variable batches
 
 }
