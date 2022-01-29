a34abcc75f37db03151df6db297143fbd2886526
Omar Ghoche <oghoche@pontusvision.com>
2022-01-29 12:23:54 -0300

    Added test cases for POLEs [sharepoint_pivacy_docs & sharepoint_legal_actions]

--- src/test/java/com/pontusvision/gdpr/PVSharepointDSARTests.java
+++ src/test/java/com/pontusvision/gdpr/PVSharepointDSARTests.java
@@ -5,11 +5,10 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestClassOrder;
 import org.junit.jupiter.api.TestMethodOrder;
 
-import java.util.Date;
 import java.util.concurrent.ExecutionException;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 //import static org.junit.Assert.assertEquals;
 //import static org.junit.Assert.assertNull;
@@ -107,80 +106,82 @@ public class PVSharepointDSARTests extends AppTest {
 
   }
 
-//  TODO ................................................................
   @Test
   public void test00002SharepointPrivacyDocs() throws InterruptedException {
 
-    jsonTestUtil("pv-extract-sharepoint-mapeamento-de-processo.json",
-            "$.queryResp[*].fields", "sharepoint_mapeamentos");
-
-    jsonTestUtil("pv-extract-sharepoint-dsar.json", "$.queryResp[*].fields", "sharepoint_dsar");
+    jsonTestUtil("non-official-pv-extract-sharepoint-privacy-docs.json", "$.queryResp[*].fields",
+            "sharepoint_privacy_docs");
 
     try {
 
-      String palmitosSARType =
-              App.executor.eval("App.g.V().has('Person.Organisation.Name', eq('PALMITOS SA'))" +
-                      ".out('Made_SAR_Request').properties('Event.Subject_Access_Request.Request_Type')" +
-                      ".value().next().toString()").get().toString();
-      assertEquals("Exclusão de e-mail", palmitosSARType, "DSAR Request type made by Palmitos SA");
+      String privacyDoc1 =
+              App.executor.eval("App.g.V().has('Object.Data_Source.Name', eq('SHAREPOINT/PRIVACY-DOCS'))" +
+                      ".out('Has_Ingestion_Event').out('Has_Ingestion_Event').out('Has_Ingestion_Event')" +
+                      ".has('Metadata.Type.Object.Privacy_Docs',eq('Object.Privacy_Docs'))" +
+                      ".values('Object.Privacy_Docs.Description').next().toString()").get().toString();
+      assertEquals("o que muda com as novas leis de segurança dos dados pessoais dos brasileiros.", privacyDoc1,
+              "Privacy Doc 1's Description.");
 
-      String palmitosSARCreateDate =
-              App.executor.eval("App.g.V().has('Person.Organisation.Name', eq('PALMITOS SA'))" +
-                      ".out('Made_SAR_Request').properties('Event.Subject_Access_Request.Metadata.Create_Date')" +
-                      ".value().next().toString()").get().toString();
-//      palmitosSARCreateDate = palmitosSARCreateDate.replaceAll("... 2021", "GMT 2021");
-      assertEquals(dtfmt.parse("Tue May 18 12:01:00 GMT 2021"), dtfmt.parse(palmitosSARCreateDate),
-              "DSAR Request type made by Palmitos SA");
+      String privacyDoc2 =
+              App.executor.eval("App.g.V().has('Object.Privacy_Docs.Form_Id',eq('2'))" +
+                      ".values('Object.Privacy_Docs.Title').next().toString()").get().toString();
+      assertEquals("Cartaz - cuide de sua identidade virtual!", privacyDoc2,"Privacy Doc 2's Title");
 
-      String completedDSARCount =
-              App.executor.eval("App.g.V().has('Event.Subject_Access_Request.Status', eq('Completed'))" +
-                      ".count().next().toString()").get().toString();
-      assertEquals("1", completedDSARCount, "Only 1 DSAR has being Completed!");
+      String privacyDoc3 =
+              App.executor.eval("App.g.V().has('Object.Privacy_Docs.Description',eq('Introdção a LGPD'))" +
+                      ".values('Object.Privacy_Docs.Date').next().toString()").get().toString();
+      assertEquals(dtfmt.parse("Mon Jan 10 01:01:01 GMT 2022"), dtfmt.parse(privacyDoc3),
+              "Privacy Doc 3's Date");
 
-      String emailChannelDSARCount =
-              App.executor.eval("App.g.V().has('Event.Subject_Access_Request.Request_Channel', " +
-                      "eq('Via e-mail')).count().next().toString()").get().toString();
-      assertEquals("2", emailChannelDSARCount, "Two DSARs where reaceived via e-mail");
-
-      String angeleBxlDSARType =
-              App.executor.eval("App.g.V().has('Person.Natural.Full_Name', eq('ANGÈLE BRUXÈLE'))" +
-                      ".out('Made_SAR_Request').properties('Event.Subject_Access_Request.Request_Type')" +
-                      ".value().next().toString()").get().toString();
-      assertEquals("Atualização de Endereço", angeleBxlDSARType,
-              "Angèle Bruxèle wants to update her Address");
-
-      String miltonOrgDSARStatus =
-              App.executor.eval("App.g.V().has('Person.Organisation.Registration_Number', " +
-                      "eq('45232190000112')).out('Made_SAR_Request')" +
-                      ".properties('Event.Subject_Access_Request.Status').value().next().toString()").get().toString();
-      assertEquals("Denied", miltonOrgDSARStatus, "Milton's Company's DSAR Request was Denied!");
-
-
-//      String DSARTotalCount =
-//              App.executor.eval("App.g.V().has('Person.Natural.Full_Name', eq('MARGORE PROXANO'))" +
-//                      ".out('Made_SAR_Request').as('DSAR').out('Has_Ingestion_Event').in('Has_Ingestion_Event')" +
-//                      ".in('Has_Ingestion_Event').out('Has_Ingestion_Event').out('Has_Ingestion_Event').dedup()" +
-//                      ".count().next().toString()").get().toString();
-//      assertEquals("4", DSARTotalCount, "Total count of DSARs: 4");
-
-      String fromDsarToRopa =
-              App.executor.eval("App.g.V().has('Person.Natural.Full_Name', eq('MARGORE PROXANO'))" +
-                      ".out('Made_SAR_Request').as('DSAR').out('Has_DSAR').as('DSAR_Group').out('Has_DSAR')" +
-                      ".as('RoPA').has('Object.Data_Procedures.Form_Id', eq('401'))" +
-                      ".values('Object.Data_Procedures.Description')" +
-                      ".next().toString()").get().toString();
-      assertEquals("Recebimento dos dados via e-mail e envio ao departamentos: Jxxxyyy e Fzzzzzzzzzz.",
-              fromDsarToRopa, "RoPA's Data Procedures' Description");
-
-      String fromDsarToRopa2 =
-              App.executor.eval("App.g.V().has('Person.Natural.Full_Name', eq('ANGÈLE BRUXÈLE'))" +
-                              ".out('Made_SAR_Request').as('DSAR').out('Has_DSAR').as('DSAR_Group').out('Has_DSAR')" +
-                              ".as('RoPA').has('Object.Data_Procedures.Form_Id', eq('402'))" +
-                              ".values('Object.Data_Procedures.Interested_Parties_Consulted').next().toString()")
-                      .get().toString();
-      assertEquals("Jurídico Snowymountain", fromDsarToRopa2,"RoPA's Interested Parties");
+      String privacyDoc4 =
+              App.executor.eval("App.g.V().has('Object.Privacy_Docs.Description',eq('Introdção a LGPD'))" +
+                      ".values('Object.Privacy_Docs.URL').next().toString()").get().toString();
+      assertEquals("www.wikipedia.com.br",privacyDoc4,"Privacy Doc 4's URL");
 
     } catch (Exception e) {
+      e.printStackTrace();
+      assertNull(e);
+    }
+
+  }
+
+  // Testing for upsert of two similar json data using sharepoint_privacy_docs POLE
+  @Test
+  public void test00003UpsertSharepointPrivacyDocs() throws InterruptedException {
+    try {
+      jsonTestUtil("non-official-pv-extract-sharepoint-privacy-docs.json",
+              "$.queryResp[*].fields", "sharepoint_privacy_docs");
+
+      String countEventIngestions =
+              App.executor.eval("App.g.V().has('Event.Ingestion.Type', eq('Privacy Docs'))" +
+                      ".count().next().toString()").get().toString();
+
+      String countPrivacyDocs =
+              App.executor.eval("App.g.V().has('Event.Ingestion.Type', eq('Privacy Docs'))" +
+                      ".out('Has_Ingestion_Event').has('Metadata.Type.Object.Privacy_Docs', eq('Object.Privacy_Docs'))" +
+                      ".dedup().count().next().toString()").get().toString();
+
+      jsonTestUtil("non-official-pv-extract-sharepoint-privacy-docs.json",
+              "$.queryResp[*].fields", "sharepoint_privacy_docs");
+
+      String countEventIngestionsAgain =
+              App.executor.eval("App.g.V().has('Event.Ingestion.Type', eq('Privacy Docs'))" +
+                      ".count().next().toString()").get().toString();
+
+//      Test for duplicate data
+
+      String countPrivacyDocsAgain =
+              App.executor.eval("App.g.V().has('Event.Ingestion.Type', eq('Privacy Docs'))" +
+                      ".out('Has_Ingestion_Event').has('Metadata.Type.Object.Privacy_Docs', eq('Object.Privacy_Docs'))" +
+                      ".dedup().count().next().toString()").get().toString();
+
+//    This proves that new insertions were made the Privacy Docs Graph part
+      assertTrue(Integer.parseInt(countEventIngestionsAgain) > Integer.parseInt(countEventIngestions));
+
+//    This proves that data is still the same
+      assertTrue(Integer.parseInt(countPrivacyDocs) == Integer.parseInt(countPrivacyDocsAgain));
+
+    } catch (ExecutionException e) {
       e.printStackTrace();
       assertNull(e);
     }
--- src/test/java/com/pontusvision/gdpr/PVSharepointDataBreachTests.java
+++ src/test/java/com/pontusvision/gdpr/PVSharepointDataBreachTests.java
@@ -6,9 +6,10 @@ import org.junit.jupiter.api.TestClassOrder;
 import org.junit.jupiter.api.TestMethodOrder;
 
 import java.util.Date;
+import java.util.concurrent.ExecutionException;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 /**
  * Unit test0000 for simple App.
@@ -66,48 +67,77 @@ public class PVSharepointDataBreachTests extends AppTest {
 
   }
 
-  //  TODO ................................................................
   @Test
   public void test00002SharepointLegalActions() throws InterruptedException {
 
-    jsonTestUtil("pv-extract-sharepoint-incidentes-de-seguranca-reportados.json",
-            "$.queryResp[*].fields", "sharepoint_data_breaches");
+    jsonTestUtil("non-official-pv-extract-sharepoint-legal-actions.json",
+            "$.queryResp[*].fields", "sharepoint_legal_actions");
 
     try {
 
-      String dataBreachStatus =
-              App.executor.eval("App.g.V().has('Object.Data_Source.Name', eq('CRM-LEADS')).in('Impacted_By_Data_Breach')" +
-                      ".properties('Event.Data_Breach.Status').value().next().toString()").get().toString();
-      assertEquals("Open", dataBreachStatus, "Status for Vazamento de E-mails de Clientes");
+      String legalAction1 =
+              App.executor.eval("App.g.V().has('Object.Data_Source.Name', eq('SHAREPOINT/LEGAL-ACTIONS'))" +
+                      ".out('Has_Ingestion_Event').out('Has_Ingestion_Event').out('Has_Ingestion_Event')" +
+                      ".has('Metadata.Type.Object.Legal_Actions',eq('Object.Legal_Actions'))" +
+                      ".values('Object.Legal_Actions.Details').next().toString()").get().toString();
+      assertEquals("Processo por vazamento de dados pessoais dos clientes.", legalAction1,
+              "Detalhes da Ação Legal 1.");
 
-      String dataBreachDate =
-              App.executor.eval("App.g.V().has('Object.Data_Source.Name', eq('CRM-LEADS')).in('Impacted_By_Data_Breach')" +
-                      ".values('Event.Data_Breach.Metadata.Create_Date').next().toString()").get().toString();
+      String legalAction2 =
+              App.executor.eval("App.g.V().has('Object.Legal_Actions.Form_Id',eq('2'))" +
+                      ".values('Object.Legal_Actions.Description').next().toString()").get().toString();
+      assertEquals("Ação judicial 768", legalAction2,"Descrição da Ação Legal 2.");
 
-      Date dateObj = dtfmt.parse(dataBreachDate);
-      Date expDateObj = dtfmt.parse("Thu Aug 12 15:17:48 GMT 2021");
-      assertEquals(expDateObj, dateObj, "Time of the Data Breach");
-
-      String dataBreachSource =
-              App.executor.eval("App.g.V().has('Object.Data_Source.Name', eq('CRM-LEADS')).in('Impacted_By_Data_Breach')" +
-                      ".properties('Event.Data_Breach.Source').value().next().toString()").get().toString();
-      assertEquals("OUTLOOK, GMAIL, YAHOO MAIL", dataBreachSource, "Source for the Data Breach on Documents");
-
-      String dataSourceArray =
-              App.executor.eval("App.g.V().has('Event.Data_Breach.Description', " +
-                      "eq('VAZAMENTO DO HISTÓRICO DE NAVEGAÇÃO DOS COLABORADORES')).out('Impacted_By_Data_Breach')" +
-                      ".has('Metadata.Type.Object.Data_Source', eq('Object.Data_Source')).dedup().count().next().toString()").get().toString();
-      assertEquals("3", dataSourceArray,"This Data_Breach event has 4 data_sources: " +
-              "Histórico navegador Google Chrome / Mozilla Firefox / Microsoft Edge / Apple Safari");
-
-      String opinionsBreach =
-              App.executor.eval("App.g.V().has('Object.Data_Source.Name', eq('SHAREPOINT/DATA-BREACHES'))" +
-                      ".out('Has_Ingestion_Event').out('Has_Ingestion_Event').in('Has_Ingestion_Event')" +
-                      ".out('Impacted_By_Data_Breach').has('Object.Data_Source.Name', eq('ERP-HR'))" +
-                      ".in('Impacted_By_Data_Breach').values('Event.Data_Breach.Impact').next().toString()").get().toString();
-      assertEquals("No Impact", opinionsBreach,"Impact for breaching employees opinions");
+      String legalAction3 =
+              App.executor.eval("App.g.V().has('Object.Legal_Actions.Description',eq('Ação judicial 333'))" +
+                      ".values('Object.Legal_Actions.Date').next().toString()").get().toString();
+      assertEquals(dtfmt.parse("Wed Jan 19 01:01:01 GMT 2022"), dtfmt.parse(legalAction3),
+              "Data da Ação Legal 3.");
 
     } catch (Exception e) {
+      e.printStackTrace();
+      assertNull(e);
+    }
+
+  }
+
+  // Testing for upsert of two similar json data using sharepoint_legal_actions POLE
+  @Test
+  public void test00003UpsertSharepointLegalActions() throws InterruptedException {
+    try {
+      jsonTestUtil("non-official-pv-extract-sharepoint-legal-actions.json",
+              "$.queryResp[*].fields", "sharepoint_legal_actions");
+
+      String countEventIngestions =
+              App.executor.eval("App.g.V().has('Event.Ingestion.Type', eq('Legal Actions'))" +
+                      ".count().next().toString()").get().toString();
+
+      String countLegalActions =
+              App.executor.eval("App.g.V().has('Event.Ingestion.Type', eq('Legal Actions'))" +
+                      ".out('Has_Ingestion_Event').has('Metadata.Type.Object.Legal_Actions', eq('Object.Legal_Actions'))" +
+                      ".dedup().count().next().toString()").get().toString();
+
+      jsonTestUtil("non-official-pv-extract-sharepoint-legal-actions2.json",
+              "$.queryResp[*].fields", "sharepoint_privacy_notice");
+
+      String countEventIngestionsAgain =
+              App.executor.eval("App.g.V().has('Event.Ingestion.Type', eq('Legal Actions'))" +
+                      ".count().next().toString()").get().toString();
+
+//      Test for duplicate data
+
+      String countLegalActionsAgain =
+              App.executor.eval("App.g.V().has('Event.Ingestion.Type', eq('Legal Actions'))" +
+                      ".out('Has_Ingestion_Event').has('Metadata.Type.Object.Legal_Actions', eq('Object.Legal_Actions'))" +
+                      ".dedup().count().next().toString()").get().toString();
+
+//    This proves that new insertions were made the Legal Actions Graph part
+      assertTrue(Integer.parseInt(countEventIngestionsAgain) > Integer.parseInt(countEventIngestions));
+
+//    This proves that data is still the same
+      assertTrue(Integer.parseInt(countLegalActions) == Integer.parseInt(countLegalActionsAgain));
+
+    } catch (ExecutionException e) {
       e.printStackTrace();
       assertNull(e);
     }
