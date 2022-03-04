8f02c6b164323a575276c7108c9f664882b6e969
Omar Ghoche <oghoche@pontusvision.com>
2022-03-04 16:57:54 -0300

    wip

--- src/test/java/com/pontusvision/gdpr/PVBudibaseTests.java
+++ src/test/java/com/pontusvision/gdpr/PVBudibaseTests.java
@@ -27,8 +27,12 @@ public class PVBudibaseTests extends AppTest {
       assertEquals("", zeroInterest, "No one is interested in this Process");
 
       String numParties =
-              App.executor.eval("App.g.V().has('Object_Data_Procedures_Interested_Parties_Consulted', " +
-                      "eq('teste@email.com, teste2@mail.com, teste3@mail.com')).count().next().toString()").get().toString();
+              App.executor.eval("App.g.V().has('Object_Data_Source_Name', eq('BUDIBASE/MAPEAMENTO-DE-PROCESSO')).as('data_source')" +
+                      ".out('Has_Ingestion_Event').as('event_group')" +
+                      ".out('Has_Ingestion_Event').as('event')" +
+                      ".out('Has_Ingestion_Event').as('data_proc')" +
+                      ".has('Object_Data_Procedures_Interested_Parties_Consulted', " +
+                      "eq('teste@email.com, teste2@mail.com, teste3@mail.com')).dedup().count().next().toString()").get().toString();
       assertEquals("3", numParties, "3 registries with same parties interested");
 
       String legInterestCount=
@@ -43,30 +47,12 @@ public class PVBudibaseTests extends AppTest {
       ).get().toString();
       assertEquals("7", legInterestCount, "Jurídico Snowymountain is a Party Interested in these Data Procedures");
 
-
 //      String LIALawfulBasis =
 //              App.executor.eval("App.g.V().has('Object_Data_Procedures_Form_Id', eq('404'))" +
 //                      ".out('Has_Legitimate_Interests_Assessment')" +
 //                      ".properties('Object_Legitimate_Interests_Assessment_Lawful_Basis_Justification')" +
 //                      ".value().next().toString()").get().toString();
 //      assertEquals("Legitimo interesse", LIALawfulBasis, "Lawful Basis Justification for this LIA is => Legitimo interesse");
-//
-//      String LIAProcessingPurpose =
-//              App.executor.eval("App.g.V().has('Object_Data_Procedures_Form_Id', eq('406'))" +
-//                      ".out('Has_Legitimate_Interests_Assessment')" +
-//                      ".properties('Object_Legitimate_Interests_Assessment_Processing_Purpose').value()" +
-//                      ".next().toString()").get().toString();
-//      assertEquals("Sim", LIAProcessingPurpose, "Processing Purpose for this LIA is => SIM");
-//
-//      String LIAPersonalDataTreatment =
-//              App.executor.eval("App.g.V().has('Object_Data_Source_Name', " +
-//                      "eq('SHAREPOINT/MAPEAMENTO-DE-PROCESSOS')).out('Has_Ingestion_Event').out('Has_Ingestion_Event')" +
-//                      ".out('Has_Ingestion_Event').out('Has_Lawful_Basis_On').has('Object_Lawful_Basis_Description', " +
-//                      "eq('EXECUÇÃO DE CONTRATO OU DE PROCEDIMENTOS PRELIMINARES A CONTRATO, A PEDIDO DO TITULAR'))" +
-//                      ".in('Has_Lawful_Basis_On').out('Has_Legitimate_Interests_Assessment')" +
-//                      ".properties('Object_Legitimate_Interests_Assessment_Personal_Data_Treatment')" +
-//                      ".value().next().toString()").get().toString();
-//      assertEquals("Sim, é indispensável - processo 1", LIAPersonalDataTreatment, "Personal Data Treatment for this LIA is => Sim, é indispensável");
 
     } catch (ExecutionException e) {
       e.printStackTrace();
@@ -75,48 +61,48 @@ public class PVBudibaseTests extends AppTest {
 
   }
 
-  // Testing for upsert of two similar json data using sharepoint_meetings POLE
-  @Test
-  public void test00002UpsertSharepointPAndPDCommitteeMeetings() throws InterruptedException {
-    try {
-
-      jsonTestUtil("sharepoint/non-official-pv-extract-sharepoint-p-and-pd-committee-meetings.json",
-              "$.queryResp[*].fields", "sharepoint_meetings");
-
-      String countEventIngestions =
-              App.executor.eval("App.g.V().has('Event_Ingestion_Type', eq('P & PD Committee Meetings'))" +
-                      ".count().next().toString()").get().toString();
-
-      String countEventMeetings =
-              App.executor.eval("App.g.V().has('Event_Ingestion_Type', eq('P & PD Committee Meetings'))" +
-                      ".out('Has_Ingestion_Event').has('Metadata_Type_Event_Meeting', eq('Event_Meeting'))" +
-                      ".dedup().count().next().toString()").get().toString();
-
-      jsonTestUtil("sharepoint/non-official-pv-extract-sharepoint-p-and-pd-committee-meetings2.json",
-              "$.queryResp[*].fields", "sharepoint_meetings");
-
-      String countEventIngestionsAgain =
-              App.executor.eval("App.g.V().has('Event_Ingestion_Type', eq('P & PD Committee Meetings'))" +
-                      ".count().next().toString()").get().toString();
-
-//      Test for duplicate data
-
-      String countEventMeetingsAgain =
-              App.executor.eval("App.g.V().has('Event_Ingestion_Type', eq('P & PD Committee Meetings'))" +
-                      ".out('Has_Ingestion_Event').has('Metadata_Type_Event_Meeting', eq('Event_Meeting'))" +
-                      ".dedup().count().next().toString()").get().toString();
-
-//    This proves that new insertions were made to the P&PD Committee Meetings Graph part
-      assertTrue(Integer.parseInt(countEventIngestionsAgain) > Integer.parseInt(countEventIngestions));
-
-//    This proves that data is still the same
-      assertTrue(Integer.parseInt(countEventMeetings) == Integer.parseInt(countEventMeetingsAgain));
-
-    } catch (ExecutionException e) {
-      e.printStackTrace();
-      assertNull(e);
-    }
-
-  }
+  // Testing for upsert of two similar json data using bb_mapeamento_de_processo POLE
+//  @Test
+//  public void test00002UpsertBudibaseMapeamentoDeProcesso() throws InterruptedException {
+//    try {
+//
+//      jsonTestUtil("sharepoint/non-official-pv-extract-sharepoint-p-and-pd-committee-meetings.json",
+//              "$.queryResp[*].fields", "sharepoint_meetings");
+//
+//      String countEventIngestions =
+//              App.executor.eval("App.g.V().has('Event_Ingestion_Type', eq('P & PD Committee Meetings'))" +
+//                      ".count().next().toString()").get().toString();
+//
+//      String countEventMeetings =
+//              App.executor.eval("App.g.V().has('Event_Ingestion_Type', eq('P & PD Committee Meetings'))" +
+//                      ".out('Has_Ingestion_Event').has('Metadata_Type_Event_Meeting', eq('Event_Meeting'))" +
+//                      ".dedup().count().next().toString()").get().toString();
+//
+//      jsonTestUtil("sharepoint/non-official-pv-extract-sharepoint-p-and-pd-committee-meetings2.json",
+//              "$.queryResp[*].fields", "sharepoint_meetings");
+//
+//      String countEventIngestionsAgain =
+//              App.executor.eval("App.g.V().has('Event_Ingestion_Type', eq('P & PD Committee Meetings'))" +
+//                      ".count().next().toString()").get().toString();
+//
+////      Test for duplicate data
+//
+//      String countEventMeetingsAgain =
+//              App.executor.eval("App.g.V().has('Event_Ingestion_Type', eq('P & PD Committee Meetings'))" +
+//                      ".out('Has_Ingestion_Event').has('Metadata_Type_Event_Meeting', eq('Event_Meeting'))" +
+//                      ".dedup().count().next().toString()").get().toString();
+//
+////    This proves that new insertions were made to the P&PD Committee Meetings Graph part
+//      assertTrue(Integer.parseInt(countEventIngestionsAgain) > Integer.parseInt(countEventIngestions));
+//
+////    This proves that data is still the same
+//      assertTrue(Integer.parseInt(countEventMeetings) == Integer.parseInt(countEventMeetingsAgain));
+//
+//    } catch (ExecutionException e) {
+//      e.printStackTrace();
+//      assertNull(e);
+//    }
+//
+//  }
 
 }
