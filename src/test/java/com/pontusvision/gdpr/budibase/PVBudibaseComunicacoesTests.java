4ef52d5853f4c8a26c6955f02d977cae0886ce89
Omar Ghoche <oghoche@pontusvision.com>
2022-03-08 11:52:53 -0300

    wip

--- src/test/java/com/pontusvision/gdpr/budibase/PVBudibaseComunicacoesTests.java
+++ src/test/java/com/pontusvision/gdpr/budibase/PVBudibaseComunicacoesTests.java
@@ -28,71 +28,34 @@ public class PVBudibaseComunicacoesTests extends AppTest {
     try {
 
       String privacyDoc1 =
-              App.executor.eval("App.g.V().has('Object_Data_Source_Name', eq('SHAREPOINT/PRIVACY-DOCS'))" +
-                      ".out('Has_Ingestion_Event').out('Has_Ingestion_Event').out('Has_Ingestion_Event')" +
-                      ".has('Metadata_Type_Object_Privacy_Docs',eq('Object_Privacy_Docs'))" +
-                      ".values('Object_Privacy_Docs_Description').next().toString()").get().toString();
-      assertEquals("o que muda com as novas leis de segurança dos dados pessoais dos brasileiros.", privacyDoc1,
-              "Privacy Doc 1's Description.");
-
-      String privacyDoc2 =
-              App.executor.eval("App.g.V().has('Object_Privacy_Docs_Form_Id',eq('2'))" +
-                      ".values('Object_Privacy_Docs_Title').next().toString()").get().toString();
-      assertEquals("Cartaz - cuide de sua identidade virtual!", privacyDoc2,"Privacy Doc 2's Title");
-
-      String privacyDoc3 =
-              App.executor.eval("App.g.V().has('Object_Privacy_Docs_Description',eq('Introdção a LGPD'))" +
-                      ".values('Object_Privacy_Docs_Date').next().toString()").get().toString();
-      assertEquals(dtfmt.parse("Mon Jan 10 01:01:01 GMT 2022"), dtfmt.parse(privacyDoc3),
-              "Privacy Doc 3's Date");
-
-      String privacyDoc4 =
-              App.executor.eval("App.g.V().has('Object_Privacy_Docs_Description',eq('Introdção a LGPD'))" +
-                      ".values('Object_Privacy_Docs_URL').next().toString()").get().toString();
-      assertEquals("www.wikipedia.com.br",privacyDoc4,"Privacy Doc 4's URL");
-
-    } catch (Exception e) {
-      e.printStackTrace();
-      assertNull(e);
-    }
-
-  }
-
-  @Test
-  public void test00001BudibasePoliticas() throws InterruptedException {
-
-    jsonTestUtil("budibase/bb-politicas.json", "$.rows", "bb_politicas");
-
-    try {
-
-      String policyText =
-              App.executor.eval("App.g.V().has('Event_Ingestion_Type'" +
-                      ",eq('budibase/políticas')).as('event-ingestion')" +
-                      ".in('Has_Ingestion_Event').as('policies')" +
-                      ".has('Object_Policies_Form_Id'" +
-                      ",eq('ro_ta_d40a2a1b6e6840e59214e15a3bd487cd_b369526f21b94395a0d98b7b79961639'))" +
-                      ".values('Object_Policies_Text').next().toString()").get().toString();
-      assertEquals("A partir do momento que um Dado Pessoal é necessário para realização de determinado processo ...",
-              policyText, "This policy's text.");
-
-      String policyType =
-              App.executor.eval("App.g.V().has('Event_Ingestion_Type'" +
-                      ",eq('budibase/políticas')).as('event-ingestion')" +
-                      ".in('Has_Ingestion_Event').as('policies')" +
-                      ".has('Object_Policies_Form_Id'" +
-                      ",eq('ro_ta_d40a2a1b6e6840e59214e15a3bd487cd_6be56a3bbd294dc88f9367fb7734d961'))" +
-                      ".values('Object_Policies_Type').next().toString()").get().toString();
-      assertEquals("Privacidade e Proteção de Dados Pessoais - Site",
-              policyType, "This policy's type.");
-
-      String dataSourceName =
-              App.executor.eval("App.g.V().has('Object_Policies_Form_Id'" +
-                      ",eq('ro_ta_d40a2a1b6e6840e59214e15a3bd487cd_6be56a3bbd294dc88f9367fb7734d961'))" +
+              App.executor.eval("App.g.V().has('Object_Data_Source_Name'" +
+                      ",eq('BUDIBASE/COMUNICAÇÕES-P&PD')).as('data-source')" +
+                      ".out('Has_Ingestion_Event').as('event-group-ingestion')" +
                       ".out('Has_Ingestion_Event').as('event-ingestion')" +
-                      ".in('Has_Ingestion_Event').as('group-ingestion')" +
-                      ".in('Has_Ingestion_Event').as('data-source')" +
-                      ".values('Object_Data_Source_Name').next().toString()").get().toString();
-      assertEquals("BUDIBASE/POLÍTICAS",dataSourceName, "This policy's type.");
+                      ".out('Has_Ingestion_Event').as('privacy-docs').has('Object_Privacy_Docs_Form_Id'" +
+                      ",eq('ro_ta_d396b9320fca45b68aa935379ca0bcd9_5794c115f8624d0cb69bd15356a120cd'))" +
+                      ".values('Object_Privacy_Docs_Description').next().toString()").get().toString();
+      assertEquals("teste", privacyDoc1, "Privacy Doc 1's Description.");
+
+      String privacyDocDate =
+              App.executor.eval("App.g.V().has('Object_Data_Source_Name'" +
+                      ",eq('BUDIBASE/COMUNICAÇÕES-P&PD')).as('data-source')" +
+                      ".out('Has_Ingestion_Event').as('event-group-ingestion')" +
+                      ".out('Has_Ingestion_Event').as('event-ingestion')" +
+                      ".out('Has_Ingestion_Event').as('privacy-docs').has('Object_Privacy_Docs_Form_Id'" +
+                      ",eq('ro_ta_d396b9320fca45b68aa935379ca0bcd9_5794c115f8624d0cb69bd15356a120cd'))" +
+                      ".values('Object_Privacy_Docs_Date').next().toString()").get().toString();
+      assertEquals(dtfmt.parse("Tue Feb 08 15:00:00 UTC 2022"), dtfmt.parse(privacyDocDate),"Privacy Doc 2's Title");
+
+      String privacyDocTitle =
+                      App.executor.eval("App.g.V().has('Object_Data_Source_Name'" +
+                              ",eq('BUDIBASE/COMUNICAÇÕES-P&PD')).as('data-source')" +
+                              ".out('Has_Ingestion_Event').as('event-group-ingestion')" +
+                              ".out('Has_Ingestion_Event').as('event-ingestion')" +
+                              ".out('Has_Ingestion_Event').as('privacy-docs').has('Object_Privacy_Docs_Form_Id'" +
+                              ",eq('ro_ta_d396b9320fca45b68aa935379ca0bcd9_5794c115f8624d0cb69bd15356a120cd'))" +
+                              ".values('Object_Privacy_Docs_Title').next().toString()").get().toString();
+      assertEquals("folheto LGPD",privacyDocTitle,"Privacy Docs' Title");
 
     } catch (Exception e) {
       e.printStackTrace();
@@ -101,40 +64,40 @@ public class PVBudibaseComunicacoesTests extends AppTest {
 
   }
 
-  // Testing for upsert of two similar json data using bb_politicas POLE
+  // Testing for upsert of two similar json data using bb_comunicacoes_ppd POLE
   @Test
   public void test00002UpsertBudibasePoliticas() throws InterruptedException {
     try {
 
-      jsonTestUtil("budibase/bb-politicas.json", "$.rows", "bb_politicas");
+      jsonTestUtil("budibase/bb-comunicacoes-ppd.json", "$.rows", "bb_comunicacoes_ppd");
 
       String countEventIngestions =
-              App.executor.eval("App.g.V().has('Event_Ingestion_Type', eq('budibase/políticas'))" +
+              App.executor.eval("App.g.V().has('Event_Ingestion_Type', eq('budibase/comunicações-p&pd'))" +
                       ".count().next().toString()").get().toString();
 
-      String countDataBreach =
-              App.executor.eval("App.g.V().has('Event_Ingestion_Type', eq('budibase/políticas'))" +
+      String countPrivacyDocs =
+              App.executor.eval("App.g.V().has('Event_Ingestion_Type', eq('budibase/comunicações-p&pd'))" +
                       ".out('Has_Ingestion_Event').has('Metadata_Type_Object_Policies', eq('Object_Policies'))" +
                       ".dedup().count().next().toString()").get().toString();
 
-      jsonTestUtil("budibase/bb-politicas-2.json", "$.rows", "bb_politicas");
+      jsonTestUtil("budibase/bb-comunicacoes-ppd-2.json", "$.rows", "bb_comunicacoes_ppd");
 
       String countEventIngestionsAgain =
-              App.executor.eval("App.g.V().has('Event_Ingestion_Type', eq('budibase/políticas'))" +
+              App.executor.eval("App.g.V().has('Event_Ingestion_Type', eq('budibase/comunicações-p&pd'))" +
                       ".count().next().toString()").get().toString();
 
 //      Test for duplicate data
 
-      String countDataBreachAgain =
-              App.executor.eval("App.g.V().has('Event_Ingestion_Type', eq('budibase/políticas'))" +
+      String countPrivacyDocsAgain =
+              App.executor.eval("App.g.V().has('Event_Ingestion_Type', eq('budibase/comunicações-p&pd'))" +
                       ".out('Has_Ingestion_Event').has('Metadata_Type_Object_Policies', eq('Object_Policies'))" +
                       ".dedup().count().next().toString()").get().toString();
 
-//    This proves that new insertions were made to the budibase/políticas Graph part
+//    This proves that new insertions were made to the budibase/comunicações-p&pd Graph part
       assertTrue(Integer.parseInt(countEventIngestionsAgain) > Integer.parseInt(countEventIngestions));
 
 //    This proves that data is still the same
-      assertTrue(Integer.parseInt(countDataBreach) == Integer.parseInt(countDataBreachAgain));
+      assertTrue(Integer.parseInt(countPrivacyDocs) == Integer.parseInt(countPrivacyDocsAgain));
 
     } catch (ExecutionException e) {
       e.printStackTrace();
