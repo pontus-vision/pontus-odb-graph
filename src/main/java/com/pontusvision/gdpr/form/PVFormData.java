a22a8e4b16de59dc455d359ab4cb52881817ce63
Leonardo Martins <lmartins@pontusnetworks.com>
2022-07-11 14:54:04 +0100

    2022 07 02 md2 card 171 (#200)
    * Fixing memory issues + adding jquery form backend
    * added form CRUD capabilities, plus test cases

--- src/main/java/com/pontusvision/gdpr/form/PVFormData.java
+++ src/main/java/com/pontusvision/gdpr/form/PVFormData.java
@@ -1,5 +1,7 @@
 package com.pontusvision.gdpr.form;
 
+import java.util.Arrays;
+
 public class PVFormData {
   String type;
   String subtype;
@@ -26,190 +28,246 @@ public class PVFormData {
   Long step;
   String[] userData;
 
+  public PVFormData (){
+
+  }
+  public PVFormData(PVFormData other) {
+    this.type = other.type;
+    this.subtype = other.subtype;
+    this.labels = other.labels;
+    this.className = other.className;
+    this.name = other.name;
+    this.access = other.access;
+    this.requireValidOption = other.requireValidOption;
+    this.inline = other.inline;
+    this.multiple = other.multiple;
+    if (other.values != null) {
+      this.values = Arrays.copyOf(other.values, other.values.length);
+    }
+    this.other = other.other;
+    this.description = other.description;
+    this.placeholder = other.placeholder;
+    this.value = other.value;
+    this.maxlength = other.maxlength;
+    this.rows = other.rows;
+    this.role = other.role;
+    this.toggle = other.toggle;
+    this.required = other.required;
+    this.min = other.min;
+    this.max = other.max;
+    this.step = other.step;
+    if (other.userData != null) {
+      this.userData = Arrays.copyOf(other.userData, other.userData.length);
+    }
+  }
+
   public String getType() {
     return type;
   }
 
-  public void setType(String types) {
+  public PVFormData setType(String types) {
     this.type = type;
+    return this;
   }
 
   public String getSubtype() {
     return subtype;
   }
 
-  public void setSubtype(String subtype) {
+  public PVFormData setSubtype(String subtype) {
     this.subtype = subtype;
+    return this;
   }
 
   public String getLabels() {
     return labels;
   }
 
-  public void setLabels(String labels) {
+  public PVFormData setLabels(String labels) {
     this.labels = labels;
+    return this;
   }
 
   public String getClassName() {
     return className;
   }
 
-  public void setClassName(String className) {
+  public PVFormData setClassName(String className) {
     this.className = className;
+    return this;
   }
 
   public String getName() {
-    if (name.startsWith("#")|| name.startsWith("@")){
+    if (name.startsWith("#") || name.startsWith("@")) {
       return name.substring(1);
     }
     return name;
   }
 
-  public void setName(String name) {
+  public PVFormData setName(String name) {
     this.name = name;
+    return this;
   }
 
   public Boolean getAccess() {
     return access;
   }
 
-  public void setAccess(Boolean access) {
+  public PVFormData setAccess(Boolean access) {
     this.access = access;
+    return this;
   }
 
   public Boolean getRequireValidOption() {
     return requireValidOption;
   }
 
-  public void setRequireValidOption(Boolean requireValidOption) {
+  public PVFormData setRequireValidOption(Boolean requireValidOption) {
     this.requireValidOption = requireValidOption;
+    return this;
   }
 
   public Boolean getInline() {
     return inline;
   }
 
-  public void setInline(Boolean inline) {
+  public PVFormData setInline(Boolean inline) {
     this.inline = inline;
+    return this;
   }
 
   public Boolean getMultiple() {
     return multiple;
   }
 
-  public void setMultiple(Boolean multiple) {
+  public PVFormData setMultiple(Boolean multiple) {
     this.multiple = multiple;
+    return this;
   }
 
   public PVFormDataValues[] getValues() {
     return values;
   }
 
-  public void setValues(PVFormDataValues[] values) {
+  public PVFormData setValues(PVFormDataValues[] values) {
     this.values = values;
+    return this;
   }
 
   public Boolean getOther() {
     return other;
   }
 
-  public void setOther(Boolean other) {
+  public PVFormData setOther(Boolean other) {
     this.other = other;
+    return this;
   }
 
   public String getDescription() {
     return description;
   }
 
-  public void setDescription(String description) {
+  public PVFormData setDescription(String description) {
     this.description = description;
+    return this;
   }
 
   public String getPlaceholder() {
     return placeholder;
   }
 
-  public void setPlaceholder(String placeholder) {
+  public PVFormData setPlaceholder(String placeholder) {
     this.placeholder = placeholder;
+    return this;
   }
 
   public String getValue() {
     return value;
   }
 
-  public void setValue(String value) {
+  public PVFormData setValue(String value) {
     this.value = value;
+    return this;
   }
 
   public Long getMaxlength() {
     return maxlength;
   }
 
-  public void setMaxlength(Long maxlength) {
+  public PVFormData setMaxlength(Long maxlength) {
     this.maxlength = maxlength;
+    return this;
   }
 
   public Long getRows() {
     return rows;
   }
 
-  public void setRows(Long rows) {
+  public PVFormData setRows(Long rows) {
     this.rows = rows;
+    return this;
   }
 
   public String getRole() {
     return role;
   }
 
-  public void setRole(String role) {
+  public PVFormData setRole(String role) {
     this.role = role;
+    return this;
   }
 
   public Boolean getToggle() {
     return toggle;
   }
 
-  public void setToggle(Boolean toggle) {
+  public PVFormData setToggle(Boolean toggle) {
     this.toggle = toggle;
+    return this;
   }
 
   public Boolean getRequired() {
     return required;
   }
 
-  public void setRequired(Boolean required) {
+  public PVFormData setRequired(Boolean required) {
     this.required = required;
+    return this;
   }
 
   public Long getMin() {
     return min;
   }
 
-  public void setMin(Long min) {
+  public PVFormData setMin(Long min) {
     this.min = min;
+    return this;
   }
 
   public Long getMax() {
     return max;
   }
 
-  public void setMax(Long max) {
+  public PVFormData setMax(Long max) {
     this.max = max;
+    return this;
   }
 
   public Long getStep() {
     return step;
   }
 
-  public void setStep(Long step) {
+  public PVFormData setStep(Long step) {
     this.step = step;
+    return this;
   }
 
   public String[] getUserData() {
     return userData;
   }
 
-  public void setUserData(String[] userData) {
+  public PVFormData setUserData(String[] userData) {
     this.userData = userData;
+    return this;
   }
 }
