ab5bfe2f0d733e8f69eb0095f03c2a8c723603fd
OmarGhoche <omarghoucher@gmail.com>
2022-08-26 09:11:15 -0300

    toUpperCased RoPA ID & Name Budibase

--- src/test/java/com/pontusvision/gdpr/PVTemplateTests.java
+++ src/test/java/com/pontusvision/gdpr/PVTemplateTests.java
@@ -532,16 +532,16 @@ public class PVTemplateTests extends AppTest {
 
       String report = new String(Base64.getDecoder().decode(renderReply.getBase64Report().getBytes()));
 
-      String expectedReport = "Gestão de Rede de Distribuidores-Nome, CPF, RG, Endereço, E-mail, Ocupação\n" +
-              "Gerenciamento de prestadores de serviços externos-CPF, Nome, RG, Ocupação, E-mail, Endereço\n" +
+      String expectedReport = "123-Nome do Usuário, E-mail Corporativo\n" +
+              "CADASTRAMENTO DE CLIENTES PJ-E-mail Corporativo, Nome da Empresa, Telefone, Dados Bancários\n" +
+              "GERENCIAMENTO DE PRESTADORES DE SERVIÇOS EXTERNOS-CPF, Nome, RG, Ocupação, E-mail, Endereço\n" +
+              "GERENCIAMENTO DO E-MAIL MARKETING - CLIENTES-Endereço, Nome, Telefone, Ocupação, E-mail\n" +
+              "GESTÃO DE ACESSO (RECEPÇÃO)-RG, Nome\n" +
+              "GESTÃO DE CURRÍCULOS-Endereço, Nome Completo, Data de Nascimento, Estado Civil, E-mail\n" +
+              "GESTÃO DE LEADS - SITE-E-mail, Nome Completo, Telefone, Ocupação, Estado Civil\n" +
+              "Gestão de Rede de Distribuidores-Nome, CPF, RG, Endereço, E-mail, Ocupação\n" +
               "Gestão de ferramenta gerencial (PowerBI)-Nome, CPF, RG, Endereço, E-mail, Ocupação\n" +
-              "Gestão de acesso (recepção)-RG, Nome\n" +
-              "Cadastramento de Clientes PJ-E-mail Corporativo, Nome da Empresa, Telefone, Dados Bancários\n" +
-              "Gestão de Currículos-Endereço, Nome Completo, Data de Nascimento, Estado Civil, E-mail\n" +
-              "Gerenciamento do E-mail Marketing - clientes-Endereço, Nome, Telefone, Ocupação, E-mail\n" +
-              "123-Nome do Usuário, E-mail Corporativo\n" +
-              "Homologação de Fornecedores-Nome, CPF, RG, Telefone, Endereço, E-mail, Responsáveis Legais da Empresa Fornecedora\n" +
-              "Gestão de Leads - Site-E-mail, Nome Completo, Telefone, Ocupação, Estado Civil\n";
+              "HOMOLOGAÇÃO DE FORNECEDORES-Nome, CPF, RG, Telefone, Endereço, E-mail, Responsáveis Legais da Empresa Fornecedora\n";
       assertEquals(sortLines(expectedReport), sortLines(report), "Expecting ROPA to have a Lawful Basis");
 
 
