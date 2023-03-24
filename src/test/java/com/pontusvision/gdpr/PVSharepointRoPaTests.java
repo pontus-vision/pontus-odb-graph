ef236a48a6f564666c75751c4d8c75be5e008f2a
OmarGhoche <omarghoucher@gmail.com>
2023-03-24 11:40:32 -0300

    updated POLE SV Mapeamento de Processos (#293)
    * separated POLE SV mapeamentos and LIA, now they are isolated

--- src/test/java/com/pontusvision/gdpr/PVSharepointRoPaTests.java
+++ src/test/java/com/pontusvision/gdpr/PVSharepointRoPaTests.java
@@ -53,49 +53,10 @@ public class PVSharepointRoPaTests extends AppTest {
                       "eq('Jurídico Snowymountain')).count().next().toString()").get().toString();
       assertEquals("4", numJuridicoParties, "4 registries have Jurídico Snowymountain as an Interested Party");
 
-      String LIALawfulBasis =
-              App.executor.eval("App.g.V().has('Object_Data_Procedures_Form_Id', eq('404'))" +
-                      ".out('Has_Legitimate_Interests_Assessment')" +
-                      ".properties('Object_Legitimate_Interests_Assessment_Lawful_Basis_Justification')" +
-                      ".value().next().toString()").get().toString();
-      assertEquals("Legitimo interesse", LIALawfulBasis, "Lawful Basis Justification for this LIA is => Legitimo interesse");
-
-//      String LIAProcessingPurpose =
-//              App.executor.eval("App.g.V().has('Object_Data_Procedures_Form_Id', eq('406'))" +
-//                      ".out('Has_Legitimate_Interests_Assessment')" +
-//                      ".properties('Object_Legitimate_Interests_Assessment_Is_Required').value()" +
-//                      ".next().toString()").get().toString();
-//      assertFalse(true,"Processing Purpose for this LIA is empty because this RoPA has no Legítimo interesse as a lawful basis");
-
-//      String LIAPersonalDataTreatment =
-//              App.executor.eval("App.g.V().has('Object_Data_Source_Name', " +
-//                      "eq('SHAREPOINT/MAPEAMENTO-DE-PROCESSOS')).out('Has_Ingestion_Event').out('Has_Ingestion_Event')" +
-//                      ".out('Has_Ingestion_Event').out('Has_Lawful_Basis_On').has('Object_Lawful_Basis_Description', " +
-//                      "eq('LEGÍTIMO INTERESSE')).in('Has_Lawful_Basis_On').out('Has_Legitimate_Interests_Assessment')" +
-//                      ".properties('Object_Legitimate_Interests_Assessment_Is_Essential')" +
-//                      ".value().next().toString()").get().toString();
-//      assertEquals("Sim, é indispensável - processo 4", LIAPersonalDataTreatment, " is => Sim, é indispensável");
-
 //    new AGgrid test style ------------------------------------------------------------------------------------------------------------
-      String svRopaRid = gridWrapperGetRid("[\n" +
-          "  {\n" +
-          "    \"colId\": \"Object_Data_Procedures_ID\",\n" +
-          "    \"filterType\": \"text\",\n" +
-          "    \"type\": \"equals\",\n" +
-          "    \"filter\": \"4\"\n" +
-          "  }\n" +
-          "]", "Object_Data_Procedures",
-        new String[]{"Object_Data_Procedures_ID"});
-
-      RecordReply reply = gridWrapper(null, "Object_Legitimate_Interests_Assessment",
-              new String[]{"Object_Legitimate_Interests_Assessment_Is_Essential"}, "hasNeighbourId:" + svRopaRid);
-      String replyStr = reply.getRecords()[0];
-
-      assertTrue(replyStr.contains("\"Object_Legitimate_Interests_Assessment_Is_Essential\":\"Sim, é indispensável - processo 4\""));
-// -------------------------------------------------------------------------------------------------------------------------------------
 
 //    testing new edge between Object_Email ------- Is_Responsible ------- > RoPA
-      svRopaRid = gridWrapperGetRid("[\n" +
+     String svRopaRid = gridWrapperGetRid("[\n" +
           "  {\n" +
           "    \"colId\": \"Object_Data_Procedures_Form_Id\",\n" +
           "    \"filterType\": \"text\",\n" +
@@ -105,9 +66,9 @@ public class PVSharepointRoPaTests extends AppTest {
           "]", "Object_Data_Procedures",
         new String[]{"Object_Data_Procedures_ID"});
 
-      reply = gridWrapper(null, "Object_Email_Address", new String[]{"Object_Email_Address_Email"},
+     RecordReply reply = gridWrapper(null, "Object_Email_Address", new String[]{"Object_Email_Address_Email"},
         "hasNeighbourId:" + svRopaRid);
-      replyStr = reply.getRecords()[0];
+     String replyStr = reply.getRecords()[0];
 
       assertTrue(replyStr.contains("\"Object_Email_Address_Email\":\"bill@outlook.com\""), "Responsible for RoPA 401");
 
